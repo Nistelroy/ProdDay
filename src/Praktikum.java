1f750efdfdb6d1b68aac88710e4db150b71be7ee
Nistel <duz3000@gmail.com>
2023-09-26 19:02:52 +0300

    fix stile

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -5,19 +5,17 @@ public class Praktikum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год:");
+
         while (true){
         int year = scanner.nextInt();
-
-                if (isLeapYear(year)) {
-                    System.out.println("13.09."+year+".");
-                } else {
-                    System.out.println("12.09."+year+".");
-                }
-            }
-    }
-            public static boolean isLeapYear(int year) {
-                if (year %4 == 0){
-                return true;
-                } else return false;
-            }
+             if (isLeapYear(year)) {
+                 System.out.println("13.09."+year+".");
+             } else {
+                 System.out.println("12.09."+year+".");
+             }
         }
+    }
+    public static boolean isLeapYear(int year) {
+        return year %4 == 0;
+    }
+}
